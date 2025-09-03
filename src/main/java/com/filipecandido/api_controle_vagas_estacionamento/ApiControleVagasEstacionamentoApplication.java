fc5230c10a29f5b795e6ad77f119f3913dc9a51a
Filipe <filipe.candido.rodrigues@gmail.com>
2025-09-02 23:08:24 -0300

    primeiro commit

---
+++ src/main/java/com/filipecandido/api_controle_vagas_estacionamento/ApiControleVagasEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.filipecandido.api_controle_vagas_estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleVagasEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleVagasEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipecandido/api_controle_vagas_estacionamento/ApiControleVagasEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipecandido.api_controle_vagas_estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleVagasEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
